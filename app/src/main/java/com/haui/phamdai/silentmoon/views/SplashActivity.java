0bb2bf024a7faddce7f9db556ee2216a64051776
phamdaiyb98 <44748066+phamdaiyb98@users.noreply.github.com>
2021-11-08 16:23:47 +0700

    Add android project

--- app/src/main/java/com/haui/phamdai/silentmoon/views/SplashActivity.java
+++ app/src/main/java/com/haui/phamdai/silentmoon/views/SplashActivity.java
@@ -1,10 +1,13 @@
 package com.haui.phamdai.silentmoon.views;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 
 import com.haui.phamdai.silentmoon.R;
@@ -13,6 +16,7 @@ import java.util.Calendar;
 
 public class SplashActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,21 @@ public class SplashActivity extends AppCompatActivity {
         btnGetStarted.setOnClickListener(v -> {
             Intent intent = new Intent(this, SignInAndSignUpActivity.class);
             startActivity(intent);
+//            startWifiQRCodeScanner(this);
         });
     }
+
+//    test easy qr scanner
+//    @RequiresApi(api = Build.VERSION_CODES.Q)
+//    private void startWifiQRCodeScanner(Context context)
+//    {
+//        final String INTENT_ACTION_WIFI_QR_SCANNER = "android.settings.WIFI_DPP_ENROLLEE_QR_CODE_SCANNER";
+//        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//
+//        if(wifiManager.isEasyConnectSupported())
+//        {
+//            final Intent intent = new Intent(INTENT_ACTION_WIFI_QR_SCANNER);
+//            startActivityForResult(intent, 5000);
+//        }
+//    }
 }
